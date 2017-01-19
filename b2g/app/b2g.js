d325e8abfd9a35b499bbabafa6f6ba6e72b42cf9
Shelly Lin <shelly.lin@kaiostech.com>
2017-01-19 15:49:52 +0800

    Bug 7847 - Add comments for GLCursor module. r=seinlin

--- b2g/app/b2g.js
+++ b2g/app/b2g.js
@@ -1163,4 +1163,6 @@ pref("extensions.blocklist.detailsURL", "https://www.mozilla.com/%LOCALE%/blockl
 // format as https://maps.googleapis.com/maps/api/geocode/json?latlng=<lat>,<lon>&key=<key>
 pref("google.geocoding.URL", "https://maps.googleapis.com/maps/api/geocode/json?latlng=");
 
+// GLCursor draws a cursor image if widget receives eMouseMove event. It is
+// usually enabled with SpatialNavigation.
 pref("gfx.glcursor.enabled", false);
