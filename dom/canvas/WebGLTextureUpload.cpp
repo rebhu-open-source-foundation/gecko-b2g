overholt/firefox

Bug 1333858 - Intersect should be fallible on overflow. r=daoshengmu, a=jcristau
MozReview-Commit-ID: 6lmIKKyXXah

dom/canvas/WebGLTextureUpload.cpp
@@ -1986,11 +1986,15 @@ DoCopyTexOrSubImage(WebGLContext* webgl, const char* funcName, bool isSubImage,
 
     ////
 
-    uint32_t readX, readY;
-    uint32_t writeX, writeY;
-    uint32_t rwWidth, rwHeight;
-    Intersect(srcTotalWidth, xWithinSrc, dstWidth, &readX, &writeX, &rwWidth);
-    Intersect(srcTotalHeight, yWithinSrc, dstHeight, &readY, &writeY, &rwHeight);
+    int32_t readX, readY;
+    int32_t writeX, writeY;
+    int32_t rwWidth, rwHeight;
+    if (!Intersect(srcTotalWidth, xWithinSrc, dstWidth, &readX, &writeX, &rwWidth) ||
+        !Intersect(srcTotalHeight, yWithinSrc, dstHeight, &readY, &writeY, &rwHeight))
+    {
+        webgl->ErrorOutOfMemory("%s: Bad subrect selection.", funcName);
+        return false;
+    }
 
     writeX += xOffset;
     writeY += yOffset;
@@ -2003,7 +2007,7 @@ DoCopyTexOrSubImage(WebGLContext* webgl, const char* funcName, bool isSubImage,
         if (!isSubImage) {
             UniqueBuffer buffer;
 
-            if (rwWidth != dstWidth || rwHeight != dstHeight) {
+            if (uint32_t(rwWidth) != dstWidth || uint32_t(rwHeight) != dstHeight) {
                 const auto& pi = idealUnpack->ToPacking();
                 CheckedUint32 byteCount = BytesPerPixel(pi);
                 byteCount *= dstWidth;

rebhu-open-source-foundation/gecko-b2g

--HG--
extra : rebase_source : 9f85a3401aa394e513878288c88795674de188da
extra : histedit_source : 0ad5cac062fe6c50927396cc0e1cb7ce228e790f
(cherry picked from commit 16030cc142cf0ca4f6b2ffccd1878a9f0fdcb921)

dom/canvas/WebGLTextureUpload.cpp
@@ -1788,14 +1788,22 @@ WebGLTexture::CopyTexImage2D(TexImageTarget target, GLint level, GLenum internal
     ScopedCopyTexImageSource maybeSwizzle(mContext, funcName, srcWidth, srcHeight,
                                           srcFormat, dstUsage);
 
-    uint32_t readX, readY;
-    uint32_t writeX, writeY;
-    uint32_t rwWidth, rwHeight;
-    Intersect(srcWidth, x, width, &readX, &writeX, &rwWidth);
-    Intersect(srcHeight, y, height, &readY, &writeY, &rwHeight);
+    ////
+
+    int32_t readX, readY;
+    int32_t writeX, writeY;
+    int32_t rwWidth, rwHeight;
+    if (!Intersect(srcWidth, x, width, &readX, &writeX, &rwWidth) ||
+        !Intersect(srcHeight, y, height, &readY, &writeY, &rwHeight))
+    {
+        mContext->ErrorOutOfMemory("%s: Bad subrect selection.", funcName);
+        return;
+    }
+
+    ////
 
     GLenum error;
-    if (rwWidth == uint32_t(width) && rwHeight == uint32_t(height)) {
+    if (rwWidth == width && rwHeight == height) {
         MOZ_ASSERT(dstUsage->idealUnpack);
         error = DoCopyTexImage2D(gl, target, level, dstUsage->idealUnpack->internalFormat, x, y, width, height,
                                  border);
@@ -1909,11 +1917,19 @@ WebGLTexture::CopyTexSubImage(const char* funcName, TexImageTarget target, GLint
     ScopedCopyTexImageSource maybeSwizzle(mContext, funcName, srcWidth, srcHeight,
                                           srcFormat, dstUsage);
 
-    uint32_t readX, readY;
-    uint32_t writeX, writeY;
-    uint32_t rwWidth, rwHeight;
-    Intersect(srcWidth, x, width, &readX, &writeX, &rwWidth);
-    Intersect(srcHeight, y, height, &readY, &writeY, &rwHeight);
+    ////
+
+    int32_t readX, readY;
+    int32_t writeX, writeY;
+    int32_t rwWidth, rwHeight;
+    if (!Intersect(srcWidth, x, width, &readX, &writeX, &rwWidth) ||
+        !Intersect(srcHeight, y, height, &readY, &writeY, &rwHeight))
+    {
+        mContext->ErrorOutOfMemory("%s: Bad subrect selection.", funcName);
+        return;
+    }
+
+    ////
 
     if (!rwWidth || !rwHeight) {
         // There aren't any, so we're 'done'.