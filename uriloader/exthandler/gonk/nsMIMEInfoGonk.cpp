5b8438cce166ae853755ac9238e2735c80eb48c0
Fabrice Desré <fabrice@desre.org>
2019-02-14 20:56:17 -0800

    Minimal uriloader/exthandler implementation for Gonk

---
+++ uriloader/exthandler/gonk/nsMIMEInfoGonk.cpp
@@ -0,0 +1,23 @@
+/* -*- Mode: C++; tab-width: 3; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#include "nsMIMEInfoGonk.h"
+
+nsresult nsMIMEInfoGonk::LoadUriInternal(nsIURI *aURI) {
+  return NS_ERROR_NOT_IMPLEMENTED;
+}
+
+NS_IMETHODIMP
+nsMIMEInfoGonk::GetHasDefaultHandler(bool *_retval) {
+  
+  *_retval = false;
+
+  return NS_OK;
+}
+
+nsresult nsMIMEInfoGonk::LaunchDefaultWithFile(nsIFile *aFile) {
+  return NS_ERROR_FILE_NOT_FOUND;
+}
---
+++ uriloader/exthandler/gonk/nsMIMEInfoGonk.h
@@ -0,0 +1,30 @@
+/* -*- Mode: C++; tab-width: 3; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#ifndef nsMIMEInfoGonk_h_
+#define nsMIMEInfoGonk_h_
+
+#include "nsMIMEInfoImpl.h"
+
+class nsMIMEInfoGonk : public nsMIMEInfoImpl {
+ public:
+  explicit nsMIMEInfoGonk(const char *aMIMEType = "")
+      : nsMIMEInfoImpl(aMIMEType) {}
+  explicit nsMIMEInfoGonk(const nsACString &aMIMEType)
+      : nsMIMEInfoImpl(aMIMEType) {}
+  nsMIMEInfoGonk(const nsACString &aType, HandlerClass aClass)
+      : nsMIMEInfoImpl(aType, aClass) {}
+  static bool HandlerExists(const char *aProtocolScheme);
+
+ protected:
+  NS_IMETHOD GetHasDefaultHandler(bool *_retval) override;
+
+  virtual nsresult LoadUriInternal(nsIURI *aURI) override;
+
+  virtual nsresult LaunchDefaultWithFile(nsIFile *aFile) override;
+};
+
+#endif  // nsMIMEInfoGonk_h_
