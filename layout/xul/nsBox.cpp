7d268dfd643dd6a0978425bfd2bd31739d74f36c
Fabrice Desré <fabrice@desre.org>
2019-02-14 21:45:09 -0800

    More theme to ignore

--- layout/xul/nsBox.cpp
+++ layout/xul/nsBox.cpp
@@ -61,10 +61,12 @@ nsBox::nsBox(ComputedStyle* aStyle, nsPresContext* aPresContext, ClassID aID)
     : nsIFrame(aStyle, aPresContext, aID) {
   MOZ_COUNT_CTOR(nsBox);
   if (!gGotTheme) {
+#ifndef MOZ_WIDGET_GONK
     gTheme = do_GetNativeTheme();
     if (gTheme) {
       gGotTheme = true;
     }
+#endif
   }
 }
 
