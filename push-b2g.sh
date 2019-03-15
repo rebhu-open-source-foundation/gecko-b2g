f81ede1a235c2a1cbf1bc0d5e43a5b4235f3d2d4
Fabrice Desré <fabrice@desre.org>
2019-03-15 16:32:37 -0700

    Helper script to push on device

---
+++ push-b2g.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -e
+
+adb root && adb remount
+
+adb shell stop b2g
+
+mkdir tmp_b2g
+cd tmp_b2g
+tar xf ../obj-arm-unknown-linux-androideabi/dist/b2g-67.0a1.en-US.linux-androideabi-arm.tar.gz
+cd b2g
+adb push * /system/b2g
+cd ../..
+rm -rf tmp_b2g
+
+# adb shell start b2g
