80aab51074e7ac2567e4eccb2bb12ad2aa5787f6
Fabrice Desré <fabrice@desre.org>
2019-08-21 14:35:38 -0700

    Bug 68846 - clean up unused MOZ_B2G_LOADER and MOZ_NUWA_PROCESS r=thinker

--- widget/gonk/GonkMemoryPressureMonitoring.cpp
+++ widget/gonk/GonkMemoryPressureMonitoring.cpp
@@ -26,10 +26,6 @@
 #define LOG(args...)  \
   __android_log_print(ANDROID_LOG_INFO, "GonkMemoryPressure" , ## args)
 
-#ifdef MOZ_NUWA_PROCESS
-#include "ipc/Nuwa.h"
-#endif
-
 using namespace mozilla;
 
 namespace {
@@ -124,12 +120,6 @@ public:
   NS_IMETHOD Run() override
   {
     MOZ_ASSERT(!NS_IsMainThread());
-
-#ifdef MOZ_NUWA_PROCESS
-    if (IsNuwaProcess()) {
-      NuwaMarkCurrentThread(nullptr, nullptr);
-    }
-#endif
 
     int triggerResetTimeout = -1;
     bool memoryPressure;
