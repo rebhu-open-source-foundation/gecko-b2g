cc7e67be62a95683733a47a49dc1d0d381844483
Brendan Dahl <bdahl@mozilla.com>
2019-11-04 16:52:35 +0000

    Bug 1470510 - Rename nsXULWindow and nsIXULWindow to AppWindow and nsIAppWindow. r=smaug
    nsXULWindow is no longer XUL specific and is somewhat confusing name.
    
    Differential Revision: https://phabricator.services.mozilla.com/D51486
    
    --HG--
    rename : xpfe/appshell/nsXULWindow.cpp => xpfe/appshell/AppWindow.cpp
    rename : xpfe/appshell/nsXULWindow.h => xpfe/appshell/AppWindow.h
    rename : xpfe/appshell/nsIXULWindow.idl => xpfe/appshell/nsIAppWindow.idl
    extra : moz-landing-system : lando

--- dom/base/nsGlobalWindowInner.cpp
+++ dom/base/nsGlobalWindowInner.cpp
@@ -161,7 +161,6 @@
 #include "mozilla/EventStateManager.h"
 #include "nsIObserverService.h"
 #include "nsFocusManager.h"
-#include "nsIXULWindow.h"
 #include "nsITimedChannel.h"
 #include "nsServiceManagerUtils.h"
 #ifdef MOZ_XUL
