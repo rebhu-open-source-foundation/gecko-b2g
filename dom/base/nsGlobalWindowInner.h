ec4a25f95ebcc65135b2d013ed6b9782097812a8
Csoregi Natalia <ncsoregi@mozilla.com>
2019-11-05 06:44:15 +0200

    Backed out changeset 9c05fa3c0fcc (bug 1575008) for bustages on WebrtcIPCTraits.h. CLOSED TREE
    --HG--
    rename : third_party/rust/arrayvec-0.4.11/build.rs => third_party/rust/arrayvec/build.rs
    rename : third_party/rust/arrayvec-0.4.11/src/maybe_uninit_nodrop.rs => third_party/rust/arrayvec/src/maybe_uninit_nodrop.rs
    rename : third_party/rust/arrayvec-0.4.11/src/maybe_uninit_stable.rs => third_party/rust/arrayvec/src/maybe_uninit_stable.rs
    rename : third_party/rust/arrayvec-0.4.11/src/range.rs => third_party/rust/arrayvec/src/range.rs

--- dom/base/nsGlobalWindowInner.h
+++ dom/base/nsGlobalWindowInner.h
@@ -48,6 +48,7 @@
 #include "mozilla/LinkedList.h"
 #include "mozilla/OwningNonNull.h"
 #include "mozilla/TimeStamp.h"
+#include "mozilla/webgpu/InstanceProvider.h"
 #include "nsWrapperCacheInlines.h"
 #include "mozilla/dom/Document.h"
 #include "mozilla/dom/EventTarget.h"
@@ -181,7 +182,8 @@ class nsGlobalWindowInner final : public mozilla::dom::EventTarget,
                                   public nsSupportsWeakReference,
                                   public nsIInterfaceRequestor,
                                   public PRCListStr,
-                                  public nsAPostRefreshObserver {
+                                  public nsAPostRefreshObserver,
+                                  public mozilla::webgpu::InstanceProvider {
  public:
   typedef mozilla::dom::BrowsingContext RemoteProxy;
 
