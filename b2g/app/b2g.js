a2ef03ddd1cca18cf61370357832aa63e724fae2
Fabrice Desré <fabrice@desre.org>
2020-04-16 14:02:21 -0700

    Bug 64598 - Enable sandboxing r=thinker.lee

--- b2g/app/b2g.js
+++ b2g/app/b2g.js
@@ -1109,7 +1109,7 @@ pref("extensions.systemAddon.update.enabled", false);
 
 pref("browser.privatebrowsing.autostart", false);
 
-//pref("security.sandbox.content.level", 3);
+pref("security.sandbox.content.level", 4);
 
 pref("gfx.e10s.font-list.shared", true);
 
