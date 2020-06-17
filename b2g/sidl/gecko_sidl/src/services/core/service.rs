1f591e6998247b654151fe6b5de85e3200d1a578
Fabrice Desré <fabrice@desre.org>
2020-06-17 12:50:43 -0700

    Bug 96725 - gecko-dev license files or strings are missing

--- b2g/sidl/gecko_sidl/src/services/core/service.rs
+++ b2g/sidl/gecko_sidl/src/services/core/service.rs
@@ -1,3 +1,7 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
 /// The "core service" client.
 use crate::common::core::{
     BaseMessage, CoreRequest, CoreResponse, DisableEventListenerRequest,
