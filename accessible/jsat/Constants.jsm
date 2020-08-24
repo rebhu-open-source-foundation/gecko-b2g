1e7204b819c2296763a98f7cea79a9ed03e18d59
martin.hsu <martin.hsu@kaiostech.com>
2020-08-24 07:53:41 +0000

    Bug 99954 part 1 - Bring back screen reader modules. r=vincent.chang

---
+++ accessible/jsat/Constants.jsm
@@ -0,0 +1,59 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+var { XPCOMUtils } = ChromeUtils.import(
+  "resource://gre/modules/XPCOMUtils.jsm"
+);
+
+this.EXPORTED_SYMBOLS = [
+  "Roles",
+  "Events",
+  "Relations",
+  "Filters",
+  "States",
+  "Prefilters",
+];
+
+function ConstantsMap(aObject, aPrefix, aMap = {}, aModifier = null) {
+  let offset = aPrefix.length;
+  for (var name in aObject) {
+    if (name.indexOf(aPrefix) === 0) {
+      aMap[name.slice(offset)] = aModifier
+        ? aModifier(aObject[name])
+        : aObject[name];
+    }
+  }
+
+  return aMap;
+}
+
+XPCOMUtils.defineLazyGetter(this, "Roles", function() {
+  return ConstantsMap(Ci.nsIAccessibleRole, "ROLE_");
+});
+
+XPCOMUtils.defineLazyGetter(this, "Events", function() {
+  return ConstantsMap(Ci.nsIAccessibleEvent, "EVENT_");
+});
+
+XPCOMUtils.defineLazyGetter(this, "Relations", function() {
+  return ConstantsMap(Ci.nsIAccessibleRelation, "RELATION_");
+});
+
+XPCOMUtils.defineLazyGetter(this, "Prefilters", function() {
+  return ConstantsMap(Ci.nsIAccessibleTraversalRule, "PREFILTER_");
+});
+
+XPCOMUtils.defineLazyGetter(this, "Filters", function() {
+  return ConstantsMap(Ci.nsIAccessibleTraversalRule, "FILTER_");
+});
+
+XPCOMUtils.defineLazyGetter(this, "States", function() {
+  let statesMap = ConstantsMap(Ci.nsIAccessibleStates, "STATE_", {}, val => {
+    return { base: val, extended: 0 };
+  });
+  ConstantsMap(Ci.nsIAccessibleStates, "EXT_STATE_", statesMap, val => {
+    return { base: 0, extended: val };
+  });
+  return statesMap;
+});
---
+++ accessible/jsat/content-script.js
@@ -0,0 +1,192 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+/* global content */
+
+const { XPCOMUtils } = ChromeUtils.import(
+  "resource://gre/modules/XPCOMUtils.jsm"
+);
+
+XPCOMUtils.defineLazyGetter(this, "Logger", function() {
+  const { Logger } = ChromeUtils.import(
+    "resource://gre/modules/accessibility/Utils.jsm"
+  );
+  return Logger;
+});
+XPCOMUtils.defineLazyGetter(this, "Presentation", function() {
+  const { Presentation } = ChromeUtils.import(
+    "resource://gre/modules/accessibility/Presentation.jsm"
+  );
+  return Presentation;
+});
+
+XPCOMUtils.defineLazyGetter(this, "Utils", function() {
+  const { Utils } = ChromeUtils.import(
+    "resource://gre/modules/accessibility/Utils.jsm"
+  );
+  return Utils;
+});
+XPCOMUtils.defineLazyGetter(this, "EventManager", function() {
+  const { EventManager } = ChromeUtils.import(
+    "resource://gre/modules/accessibility/EventManager.jsm"
+  );
+  return EventManager;
+});
+XPCOMUtils.defineLazyGetter(this, "ContentControl", function() {
+  const { ContentControl } = ChromeUtils.import(
+    "resource://gre/modules/accessibility/ContentControl.jsm"
+  );
+  return ContentControl;
+});
+XPCOMUtils.defineLazyGetter(this, "Roles", function() {
+  const { Roles } = ChromeUtils.import(
+    "resource://gre/modules/accessibility/Constants.jsm"
+  );
+  return Roles;
+});
+XPCOMUtils.defineLazyGetter(this, "States", function() {
+  const { States } = ChromeUtils.import(
+    "resource://gre/modules/accessibility/Constants.jsm"
+  );
+  return States;
+});
+
+Logger.info("content-script.js", content.document.location);
+
+var eventManager = null;
+var contentControl = null;
+
+function forwardToParent(aMessage) {
+  // XXX: This is a silly way to make a deep copy
+  let newJSON = JSON.parse(JSON.stringify(aMessage.json));
+  newJSON.origin = "child";
+  sendAsyncMessage(aMessage.name, newJSON);
+}
+
+function forwardToChild(aMessage, aListener, aVCPosition) {
+  let acc = aVCPosition || Utils.getVirtualCursor(content.document).position;
+
+  if (!Utils.isAliveAndVisible(acc) || acc.role != Roles.INTERNAL_FRAME) {
+    return false;
+  }
+
+  Logger.debug(() => {
+    return [
+      "forwardToChild",
+      Logger.accessibleToString(acc),
+      aMessage.name,
+      JSON.stringify(aMessage.json, null, "  "),
+    ];
+  });
+
+  let mm = Utils.getMessageManager(acc.DOMNode);
+
+  if (aListener) {
+    mm.addMessageListener(aMessage.name, aListener);
+  }
+
+  // XXX: This is a silly way to make a deep copy
+  let newJSON = JSON.parse(JSON.stringify(aMessage.json));
+  newJSON.origin = "parent";
+  if (Utils.isContentProcess) {
+    // XXX: OOP content's screen offset is 0,
+    // so we remove the real screen offset here.
+    newJSON.x -= content.mozInnerScreenX;
+    newJSON.y -= content.mozInnerScreenY;
+  }
+  mm.sendAsyncMessage(aMessage.name, newJSON);
+  return true;
+}
+
+function activateContextMenu(aMessage) {
+  let position = Utils.getVirtualCursor(content.document).position;
+  if (!forwardToChild(aMessage, activateContextMenu, position)) {
+    let center = Utils.getBounds(position, true).center();
+
+    let evt = content.document.createEvent("HTMLEvents");
+    evt.initEvent("contextmenu", true, true);
+    evt.clientX = center.x;
+    evt.clientY = center.y;
+    position.DOMNode.dispatchEvent(evt);
+  }
+}
+
+function presentCaretChange(aText, aOldOffset, aNewOffset) {
+  if (aOldOffset !== aNewOffset) {
+    let msg = Presentation.textSelectionChanged(
+      aText,
+      aNewOffset,
+      aNewOffset,
+      aOldOffset,
+      aOldOffset,
+      true
+    );
+    sendAsyncMessage("AccessFu:Present", msg);
+  }
+}
+
+function scroll(aMessage) {
+  let position = Utils.getVirtualCursor(content.document).position;
+  if (!forwardToChild(aMessage, scroll, position)) {
+    sendAsyncMessage("AccessFu:DoScroll", {
+      bounds: Utils.getBounds(position, true),
+      page: aMessage.json.page,
+      horizontal: aMessage.json.horizontal,
+    });
+  }
+}
+
+addMessageListener("AccessFu:Start", function(m) {
+  if (m.json.logLevel) {
+    Logger.logLevel = Logger[m.json.logLevel];
+  }
+
+  Logger.debug("AccessFu:Start");
+  if (m.json.buildApp) {
+    Utils.MozBuildApp = m.json.buildApp;
+  }
+
+  addMessageListener("AccessFu:ContextMenu", activateContextMenu);
+  addMessageListener("AccessFu:Scroll", scroll);
+
+  Utils.isBrowserFrame = this.docShell.isBrowserElement || false;
+
+  if (!contentControl) {
+    contentControl = new ContentControl(this);
+  }
+  contentControl.start();
+
+  if (!eventManager) {
+    eventManager = new EventManager(this, contentControl);
+  }
+  eventManager.inTest = m.json.inTest;
+  eventManager.start();
+
+  function contentStarted() {
+    let accDoc = Utils.AccRetrieval.getAccessibleFor(content.document);
+    if (accDoc && !Utils.getState(accDoc).contains(States.BUSY)) {
+      sendAsyncMessage("AccessFu:ContentStarted");
+    } else {
+      content.setTimeout(contentStarted, 0);
+    }
+  }
+
+  if (m.json.inTest) {
+    // During a test we want to wait for the document to finish loading for
+    // consistency.
+    contentStarted();
+  }
+});
+
+addMessageListener("AccessFu:Stop", function(m) {
+  Logger.debug("AccessFu:Stop");
+
+  removeMessageListener("AccessFu:ContextMenu", activateContextMenu);
+  removeMessageListener("AccessFu:Scroll", scroll);
+
+  eventManager.stop();
+  contentControl.stop();
+});
+
+sendAsyncMessage("AccessFu:Ready");
