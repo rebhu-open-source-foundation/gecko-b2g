ecbb3ee9555e8d2e285262635dfcde799da4dac3
Fabrice Desré <fabrice@desre.org>
2021-01-04 17:08:26 -0800

    Bug 111413 - ensure the page is reloaded when setting src again. r=xiaosong.lu

--- b2g/chrome/content/embedding/web-view.js
+++ b2g/chrome/content/embedding/web-view.js
@@ -581,6 +581,11 @@
         this.attrs.push({ name: "src", new_value: url });
       } else {
         this.browser.setAttribute("src", url);
+        // Setting the "src" attribute doesn't trigger a load if the urls
+        // are the same, so we trigger a reload instead.
+        if (url == this.browser.currentURI?.spec) {
+          this.reload();
+        }
       }
     }
 
