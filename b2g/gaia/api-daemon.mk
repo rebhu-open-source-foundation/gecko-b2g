4701c77f16bccea57db1491fb1c1746f36a1b92f
chuntang.lee <chuntang.lee@kaiostech.com>
2021-03-24 09:54:10 +0000

    Bug 119301 - Fix /bin/cargo not found build break. r=martin.hsu

--- b2g/gaia/api-daemon.mk
+++ b2g/gaia/api-daemon.mk
@@ -1,6 +1,5 @@
 define build-sidl-daemon
   echo "run run-sidl-build-daemon";
-  CARGO_HOME=${CARGO_HOME:-"${HOME}/.cargo"}
   cd $(B2G_DIR)/sidl/daemon; RUST_BACKTRACE=1 BUILD_TYPE=debug RUST_LOG=debug WS_RUNTIME_TOKEN=secrettoken ${CARGO_HOME}/bin/cargo build --features=virtual-host; cd ../../;
 endef
 
