1c9dc03d7a222940b0fb8d3c11748f960e6f8ddc
Hanson Pu <hanson.pu@kaiostech.com>
2021-08-11 07:51:19 +0000

    Revert "Bug 113785 - Report actual devicemotion event interval. r=andy.wu"
    This reverts commit a28295e86cec9469d0a115b430c1b9821a7ab2f9

--- dom/system/nsDeviceSensors.cpp
+++ dom/system/nsDeviceSensors.cpp
@@ -497,8 +497,6 @@ void nsDeviceSensors::FireDOMMotionEvent(Document* doc, EventTarget* target,
     return;
   }
 
-  double interval = (TimeStamp::Now() - mLastDOMMotionEventTime).ToMilliseconds();
-
   IgnoredErrorResult ignored;
   RefPtr<Event> event =
       doc->CreateEvent(u"DeviceMotionEvent"_ns, CallerType::System, ignored);
@@ -511,7 +509,7 @@ void nsDeviceSensors::FireDOMMotionEvent(Document* doc, EventTarget* target,
   me->InitDeviceMotionEvent(
       u"devicemotion"_ns, true, false, *mLastAcceleration,
       *mLastAccelerationIncludingGravity, *mLastRotationRate,
-      Nullable<double>(interval), Nullable<uint64_t>(timestamp));
+      Nullable<double>(DEFAULT_SENSOR_POLL), Nullable<uint64_t>(timestamp));
 
   event->SetTrusted(true);
 
