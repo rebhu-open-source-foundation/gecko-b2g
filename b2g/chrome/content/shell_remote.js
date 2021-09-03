0479bd67356dda741e5fe6fab0c648477f0791c2
Fabrice Desré <fabrice@desre.org>
2021-09-03 15:19:25 -0700

    Bug 126528 - Part 2: cleanup shell_remote.js r=seinlin.li

--- b2g/chrome/content/shell_remote.js
+++ b2g/chrome/content/shell_remote.js
@@ -11,14 +11,9 @@ const { XPCOMUtils } = ChromeUtils.import(
 const { AppConstants } = ChromeUtils.import(
   "resource://gre/modules/AppConstants.jsm"
 );
-ChromeUtils.import("resource://gre/modules/ActivitiesService.jsm");
-ChromeUtils.import("resource://gre/modules/AlarmService.jsm");
-ChromeUtils.import("resource://gre/modules/DownloadService.jsm");
-
-const isGonk = AppConstants.platform === "gonk";
 
 function debug(aStr) {
-  //console.log(`-*- ShellRemote.js: ${aStr}`);
+  // console.log(`-*- ShellRemote.js: ${aStr}`);
 }
 
 var remoteShell = {
@@ -26,7 +21,7 @@ var remoteShell = {
     let url = Services.prefs.getCharPref("b2g.multiscreen.system_remote_url");
     if (!url) {
       console.error(
-        `Please set the b2g.system_startup_url preference properly`
+        `Please set the b2g.multiscreen.system_remote_url preference properly`
       );
     }
     return url;
@@ -40,76 +35,21 @@ var remoteShell = {
   start() {
     this._started = true;
 
-    // This forces the initialization of the cookie service before we hit the
-    // network.
-    // See bug 810209
-    let cookies = Cc["@mozilla.org/cookieService;1"].getService();
-    if (!cookies) {
-      debug("No cookies service!");
-    }
-
     let startURL = this.startURL;
 
     let systemAppFrame = document.createXULElement("browser");
     systemAppFrame.setAttribute("type", "chrome");
     systemAppFrame.setAttribute("id", "systemapp");
-    systemAppFrame.setAttribute("src", "blank.html");
+    systemAppFrame.setAttribute("nodefaultsrc", "true");
 
-    debug(`Loading blank.html`);
     document.body.appendChild(systemAppFrame);
 
     this.contentBrowser = systemAppFrame;
 
-    window.addEventListener("MozAfterPaint", this);
     window.addEventListener("sizemodechange", this);
     window.addEventListener("unload", this);
 
-    let stop_url = null;
-
-    // Listen for loading events on the system app xul:browser
-    let listener = {
-      onLocationChange: (webProgress, request, location, flags) => {
-        // debug(`LocationChange: ${location.spec}`);
-      },
-
-      onProgressChange: () => {
-        // debug(`ProgressChange`);
-      },
-
-      onSecurityChange: () => {
-        // debug(`SecurityChange`);
-      },
-
-      onStateChange: (webProgress, request, stateFlags, status) => {
-        // debug(`StateChange ${stateFlags}`);
-        if (stateFlags & Ci.nsIWebProgressListener.STATE_START) {
-          if (!stop_url) {
-            stop_url = request.name;
-          }
-        }
-
-        if (stateFlags & Ci.nsIWebProgressListener.STATE_STOP) {
-          // debug(`Done loading ${request.name}`);
-          if (stop_url && request.name == stop_url) {
-            this.contentBrowser.removeProgressListener(listener);
-            this.notifyContentWindowLoaded();
-          }
-        }
-      },
-
-      onStatusChange: () => {
-        // debug(`StatusChange`);
-      },
-
-      QueryInterface: ChromeUtils.generateQI([
-        Ci.nsIWebProgressListener2,
-        Ci.nsIWebProgressListener,
-        Ci.nsISupportsWeakReference,
-      ]),
-    };
-    this.contentBrowser.addProgressListener(listener);
-
-    debug(`Setting system url to ${startURL}`);
+    debug(`Setting 2nd screen system url to ${startURL}`);
 
     this.contentBrowser.src = startURL;
   },
@@ -134,20 +74,10 @@ var remoteShell = {
           this.contentBrowser.setVisible(true);
         }
         break;
-      case "MozAfterPaint":
-        window.removeEventListener("MozAfterPaint", this);
-        break;
       case "unload":
         this.stop();
         break;
     }
-  },
-
-  // This gets called when window.onload fires on the System app content window,
-  // which means things in <html> are parsed and statically referenced <script>s
-  // and <script defer>s are loaded and run.
-  notifyContentWindowLoaded() {
-    debug("notifyContentWindowLoaded");
   },
 };
 
@@ -159,16 +89,6 @@ document.addEventListener(
       console.error("ShellRemote has already started but didn't initialize!!!");
       return;
     }
-    Services.obs.addObserver(browserWindowImpl => {
-      debug("New web embedder created.");
-      window.browserDOMWindow = browserWindowImpl;
-
-      // Notify the the shell is ready at the next event loop tick to
-      // let the embedder user a chance to add event listeners.
-      window.setTimeout(() => {
-        Services.obs.notifyObservers(window, "shell-ready");
-      }, 0);
-    }, "web-embedder-created");
 
     remoteShell.start();
   },
