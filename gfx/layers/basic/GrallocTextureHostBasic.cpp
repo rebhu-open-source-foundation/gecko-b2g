78c3e5660f76694cfe17a8b54b7923171ac9b49e
Fabrice Desré <fabrice@desre.org>
2021-11-19 10:52:56 -0800

    Gonk changes for MozBug 1741784

--- gfx/layers/basic/GrallocTextureHostBasic.cpp
+++ gfx/layers/basic/GrallocTextureHostBasic.cpp
@@ -12,89 +12,87 @@
 namespace mozilla {
 namespace layers {
 
-static gfx::SurfaceFormat
-HalFormatToSurfaceFormat(int aHalFormat, TextureFlags aFlags)
-{
+static gfx::SurfaceFormat HalFormatToSurfaceFormat(int aHalFormat,
+                                                   TextureFlags aFlags) {
   bool swapRB = bool(aFlags & TextureFlags::RB_SWAPPED);
   switch (aHalFormat) {
-  case android::PIXEL_FORMAT_BGRA_8888:
-    return swapRB ? gfx::SurfaceFormat::R8G8B8A8 : gfx::SurfaceFormat::B8G8R8A8;
-  case android::PIXEL_FORMAT_RGBA_8888:
-    return swapRB ? gfx::SurfaceFormat::B8G8R8A8 : gfx::SurfaceFormat::R8G8B8A8;
-  case android::PIXEL_FORMAT_RGBX_8888:
-    return swapRB ? gfx::SurfaceFormat::B8G8R8X8 : gfx::SurfaceFormat::R8G8B8X8;
-  case android::PIXEL_FORMAT_RGB_565:
-    return gfx::SurfaceFormat::R5G6B5_UINT16;
-  case HAL_PIXEL_FORMAT_YCbCr_422_SP:
-  case HAL_PIXEL_FORMAT_YCrCb_420_SP:
-  case HAL_PIXEL_FORMAT_YCbCr_422_I:
-  case GrallocImage::HAL_PIXEL_FORMAT_YCbCr_420_SP_TILED:
-  case GrallocImage::HAL_PIXEL_FORMAT_YCbCr_420_SP_VENUS:
-  case HAL_PIXEL_FORMAT_YV12:
+    case android::PIXEL_FORMAT_BGRA_8888:
+      return swapRB ? gfx::SurfaceFormat::R8G8B8A8
+                    : gfx::SurfaceFormat::B8G8R8A8;
+    case android::PIXEL_FORMAT_RGBA_8888:
+      return swapRB ? gfx::SurfaceFormat::B8G8R8A8
+                    : gfx::SurfaceFormat::R8G8B8A8;
+    case android::PIXEL_FORMAT_RGBX_8888:
+      return swapRB ? gfx::SurfaceFormat::B8G8R8X8
+                    : gfx::SurfaceFormat::R8G8B8X8;
+    case android::PIXEL_FORMAT_RGB_565:
+      return gfx::SurfaceFormat::R5G6B5_UINT16;
+    case HAL_PIXEL_FORMAT_YCbCr_422_SP:
+    case HAL_PIXEL_FORMAT_YCrCb_420_SP:
+    case HAL_PIXEL_FORMAT_YCbCr_422_I:
+    case GrallocImage::HAL_PIXEL_FORMAT_YCbCr_420_SP_TILED:
+    case GrallocImage::HAL_PIXEL_FORMAT_YCbCr_420_SP_VENUS:
+    case HAL_PIXEL_FORMAT_YV12:
 #if defined(MOZ_WIDGET_GONK)
-  case HAL_PIXEL_FORMAT_IMPLEMENTATION_DEFINED:
+    case HAL_PIXEL_FORMAT_IMPLEMENTATION_DEFINED:
 #endif
       // Needs convert to RGB565
       return gfx::SurfaceFormat::R5G6B5_UINT16;
-  default:
-    if (aHalFormat >= 0x100 && aHalFormat <= 0x1FF) {
-      // Reserved range for HAL specific formats.
-      // Needs convert to RGB565
-      return gfx::SurfaceFormat::R5G6B5_UINT16;
-    } else {
-      MOZ_CRASH("GFX: Unhandled HAL pixel format");
-      return gfx::SurfaceFormat::UNKNOWN; // not reached
-    }
+    default:
+      if (aHalFormat >= 0x100 && aHalFormat <= 0x1FF) {
+        // Reserved range for HAL specific formats.
+        // Needs convert to RGB565
+        return gfx::SurfaceFormat::R5G6B5_UINT16;
+      } else {
+        MOZ_CRASH("GFX: Unhandled HAL pixel format");
+        return gfx::SurfaceFormat::UNKNOWN;  // not reached
+      }
   }
 }
 
-static bool
-NeedsConvertFromYUVtoRGB565(int aHalFormat)
-{
+static bool NeedsConvertFromYUVtoRGB565(int aHalFormat) {
   switch (aHalFormat) {
-  case android::PIXEL_FORMAT_BGRA_8888:
-  case android::PIXEL_FORMAT_RGBA_8888:
-  case android::PIXEL_FORMAT_RGBX_8888:
-  case android::PIXEL_FORMAT_RGB_565:
-    return false;
-  case HAL_PIXEL_FORMAT_YCbCr_422_SP:
-  case HAL_PIXEL_FORMAT_YCrCb_420_SP:
-  case HAL_PIXEL_FORMAT_YCbCr_422_I:
-  case GrallocImage::HAL_PIXEL_FORMAT_YCbCr_420_SP_TILED:
-  case GrallocImage::HAL_PIXEL_FORMAT_YCbCr_420_SP_VENUS:
-  case HAL_PIXEL_FORMAT_YV12:
+    case android::PIXEL_FORMAT_BGRA_8888:
+    case android::PIXEL_FORMAT_RGBA_8888:
+    case android::PIXEL_FORMAT_RGBX_8888:
+    case android::PIXEL_FORMAT_RGB_565:
+      return false;
+    case HAL_PIXEL_FORMAT_YCbCr_422_SP:
+    case HAL_PIXEL_FORMAT_YCrCb_420_SP:
+    case HAL_PIXEL_FORMAT_YCbCr_422_I:
+    case GrallocImage::HAL_PIXEL_FORMAT_YCbCr_420_SP_TILED:
+    case GrallocImage::HAL_PIXEL_FORMAT_YCbCr_420_SP_VENUS:
+    case HAL_PIXEL_FORMAT_YV12:
 #if defined(MOZ_WIDGET_GONK)
-  case HAL_PIXEL_FORMAT_IMPLEMENTATION_DEFINED:
+    case HAL_PIXEL_FORMAT_IMPLEMENTATION_DEFINED:
 #endif
       return true;
-  default:
-    if (aHalFormat >= 0x100 && aHalFormat <= 0x1FF) {
-      // Reserved range for HAL specific formats.
-      return true;
-    } else {
-      MOZ_CRASH("GFX: Unhandled HAL pixel format YUV");
-      return false; // not reached
-    }
+    default:
+      if (aHalFormat >= 0x100 && aHalFormat <= 0x1FF) {
+        // Reserved range for HAL specific formats.
+        return true;
+      } else {
+        MOZ_CRASH("GFX: Unhandled HAL pixel format YUV");
+        return false;  // not reached
+      }
   }
 }
 
 GrallocTextureHostBasic::GrallocTextureHostBasic(
-  TextureFlags aFlags,
-  const SurfaceDescriptorGralloc& aDescriptor)
-  : TextureHost(aFlags)
-  , mGrallocHandle(aDescriptor)
-  , mSize(0, 0)
-  , mCropSize(0, 0)
-  , mFormat(gfx::SurfaceFormat::UNKNOWN)
-  , mIsOpaque(aDescriptor.isOpaque())
-{
-  android::GraphicBuffer* grallocBuffer = GetGraphicBufferFromDesc(mGrallocHandle).get();
+    TextureFlags aFlags, const SurfaceDescriptorGralloc& aDescriptor)
+    : TextureHost(aFlags),
+      mGrallocHandle(aDescriptor),
+      mSize(0, 0),
+      mCropSize(0, 0),
+      mFormat(gfx::SurfaceFormat::UNKNOWN),
+      mIsOpaque(aDescriptor.isOpaque()) {
+  android::GraphicBuffer* grallocBuffer =
+      GetGraphicBufferFromDesc(mGrallocHandle).get();
   MOZ_ASSERT(grallocBuffer);
 
   if (grallocBuffer) {
-    mFormat =
-      HalFormatToSurfaceFormat(grallocBuffer->getPixelFormat(),
-                               aFlags & TextureFlags::RB_SWAPPED);
+    mFormat = HalFormatToSurfaceFormat(grallocBuffer->getPixelFormat(),
+                                       aFlags & TextureFlags::RB_SWAPPED);
     mSize = gfx::IntSize(grallocBuffer->getWidth(), grallocBuffer->getHeight());
     mCropSize = mSize;
   } else {
@@ -102,9 +100,7 @@ GrallocTextureHostBasic::GrallocTextureHostBasic(
   }
 }
 
-bool
-GrallocTextureHostBasic::Lock()
-{
+bool GrallocTextureHostBasic::Lock() {
   if (!mProvider || !IsValid()) {
     return false;
   }
@@ -114,62 +110,53 @@ GrallocTextureHostBasic::Lock()
   }
 
   android::sp<android::GraphicBuffer> graphicBuffer =
-    GetGraphicBufferFromDesc(mGrallocHandle);
+      GetGraphicBufferFromDesc(mGrallocHandle);
   MOZ_ASSERT(graphicBuffer.get());
 
   RefPtr<gfx::DataSourceSurface> surf;
   if (NeedsConvertFromYUVtoRGB565(graphicBuffer->getPixelFormat())) {
     PlanarYCbCrData ycbcrData;
-    surf = GetDataSourceSurfaceFrom(graphicBuffer,
-                                    mCropSize,
-                                    ycbcrData);
+    surf = GetDataSourceSurfaceFrom(graphicBuffer, mCropSize, ycbcrData);
   } else {
     uint32_t usage = GRALLOC_USAGE_SW_READ_OFTEN;
-    int32_t rv = graphicBuffer->lock(usage,
-                                     reinterpret_cast<void**>(&mMappedBuffer));
+    int32_t rv =
+        graphicBuffer->lock(usage, reinterpret_cast<void**>(&mMappedBuffer));
     if (rv) {
       mMappedBuffer = nullptr;
       NS_WARNING("Couldn't lock graphic buffer");
       return false;
     }
     surf = gfx::Factory::CreateWrappingDataSourceSurface(
-             mMappedBuffer,
-             graphicBuffer->getStride() * gfx::BytesPerPixel(mFormat),
-             mCropSize,
-             mFormat);
+        mMappedBuffer, graphicBuffer->getStride() * gfx::BytesPerPixel(mFormat),
+        mCropSize, mFormat);
   }
   mTextureSource = mProvider->CreateDataTextureSource(mFlags);
   mTextureSource->Update(surf, nullptr);
   return true;
 }
 
-bool
-GrallocTextureHostBasic::IsValid() const
-{
-  android::GraphicBuffer* graphicBuffer = GetGraphicBufferFromDesc(mGrallocHandle).get();
+bool GrallocTextureHostBasic::IsValid() const {
+  android::GraphicBuffer* graphicBuffer =
+      GetGraphicBufferFromDesc(mGrallocHandle).get();
   return graphicBuffer != nullptr;
 }
 
-bool
-GrallocTextureHostBasic::BindTextureSource(CompositableTextureSourceRef& aTexture)
-{
+bool GrallocTextureHostBasic::BindTextureSource(
+    CompositableTextureSourceRef& aTexture) {
   aTexture = mTextureSource;
   return !!aTexture;
 }
 
-void
-GrallocTextureHostBasic::UnbindTextureSource()
-{
+void GrallocTextureHostBasic::UnbindTextureSource() {
   TextureHost::UnbindTextureSource();
   ClearTextureSource();
 }
 
-void
-GrallocTextureHostBasic::ClearTextureSource()
-{
+void GrallocTextureHostBasic::ClearTextureSource() {
   mTextureSource = nullptr;
   if (mMappedBuffer) {
-    android::GraphicBuffer* graphicBuffer = GetGraphicBufferFromDesc(mGrallocHandle).get();
+    android::GraphicBuffer* graphicBuffer =
+        GetGraphicBufferFromDesc(mGrallocHandle).get();
     MOZ_ASSERT(graphicBuffer);
     mMappedBuffer = nullptr;
     graphicBuffer->unlock();
@@ -178,8 +165,7 @@ GrallocTextureHostBasic::ClearTextureSource()
 
 void GrallocTextureHostBasic::SetTextureSourceProvider(
     TextureSourceProvider* aProvider) {
-  if (!aProvider->AsCompositor() ||
-      !aProvider->AsCompositor()->AsBasicCompositor()) {
+  if (!aProvider || !aProvider->AsBasicCompositor()) {
     mTextureSource = nullptr;
     return;
   }
@@ -191,21 +177,17 @@ void GrallocTextureHostBasic::SetTextureSourceProvider(
   }
 }
 
-gfx::SurfaceFormat
-GrallocTextureHostBasic::GetFormat() const {
+gfx::SurfaceFormat GrallocTextureHostBasic::GetFormat() const {
   return mFormat;
 }
 
-void
-GrallocTextureHostBasic::WaitAcquireFenceHandleSyncComplete()
-{
+void GrallocTextureHostBasic::WaitAcquireFenceHandleSyncComplete() {
   if (!mAcquireFenceHandle.IsValid()) {
     return;
   }
 
   RefPtr<FenceHandle::FdObj> fdObj = mAcquireFenceHandle.GetAndResetFdObj();
-  android::sp<android::Fence> fence(
-    new android::Fence(fdObj->GetAndResetFd()));
+  android::sp<android::Fence> fence(new android::Fence(fdObj->GetAndResetFd()));
 
   // Wait fece complete with timeout.
   // If a source of the fence becomes invalid because of error,
@@ -216,9 +198,7 @@ GrallocTextureHostBasic::WaitAcquireFenceHandleSyncComplete()
   }
 }
 
-void
-GrallocTextureHostBasic::SetCropRect(nsIntRect aCropRect)
-{
+void GrallocTextureHostBasic::SetCropRect(nsIntRect aCropRect) {
   MOZ_ASSERT(aCropRect.TopLeft() == gfx::IntPoint(0, 0));
   MOZ_ASSERT(!aCropRect.IsEmpty());
   MOZ_ASSERT(aCropRect.width <= mSize.width);
@@ -233,18 +213,16 @@ GrallocTextureHostBasic::SetCropRect(nsIntRect aCropRect)
   ClearTextureSource();
 }
 
-void
-GrallocTextureHostBasic::DeallocateSharedData()
-{
+void GrallocTextureHostBasic::DeallocateSharedData() {
   ClearTextureSource();
 
-  if (mGrallocHandle.buffer().type() != MaybeMagicGrallocBufferHandle::Tnull_t) {
+  if (mGrallocHandle.buffer().type() !=
+      MaybeMagicGrallocBufferHandle::Tnull_t) {
     MaybeMagicGrallocBufferHandle handle = mGrallocHandle.buffer();
     base::ProcessId owner;
     if (handle.type() == MaybeMagicGrallocBufferHandle::TGrallocBufferRef) {
       owner = handle.get_GrallocBufferRef().mOwner;
-    }
-    else {
+    } else {
       owner = handle.get_MagicGrallocBufferHandle().mRef.mOwner;
     }
 
@@ -252,25 +230,17 @@ GrallocTextureHostBasic::DeallocateSharedData()
   }
 }
 
-void
-GrallocTextureHostBasic::ForgetSharedData()
-{
-  ClearTextureSource();
-}
+void GrallocTextureHostBasic::ForgetSharedData() { ClearTextureSource(); }
 
-void
-GrallocTextureHostBasic::DeallocateDeviceData()
-{
-  ClearTextureSource();
-}
+void GrallocTextureHostBasic::DeallocateDeviceData() { ClearTextureSource(); }
 
-LayerRenderState
-GrallocTextureHostBasic::GetRenderState()
-{
-  android::GraphicBuffer* graphicBuffer = GetGraphicBufferFromDesc(mGrallocHandle).get();
+LayerRenderState GrallocTextureHostBasic::GetRenderState() {
+  android::GraphicBuffer* graphicBuffer =
+      GetGraphicBufferFromDesc(mGrallocHandle).get();
 
   if (graphicBuffer) {
-    LayerRenderStateFlags flags = LayerRenderStateFlags::LAYER_RENDER_STATE_DEFAULT;
+    LayerRenderStateFlags flags =
+        LayerRenderStateFlags::LAYER_RENDER_STATE_DEFAULT;
     if (mIsOpaque) {
       flags |= LayerRenderStateFlags::OPAQUE;
     }
@@ -280,14 +250,11 @@ GrallocTextureHostBasic::GetRenderState()
     if (mFlags & TextureFlags::RB_SWAPPED) {
       flags |= LayerRenderStateFlags::FORMAT_RB_SWAP;
     }
-    return LayerRenderState(graphicBuffer,
-                            mCropSize,
-                            flags,
-                            this);
+    return LayerRenderState(graphicBuffer, mCropSize, flags, this);
   }
 
   return LayerRenderState();
 }
 
-} // namespace layers
-} // namespace mozilla
+}  // namespace layers
+}  // namespace mozilla
